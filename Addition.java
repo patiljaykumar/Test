754f99acfbf47e7f7fcadf0458ede4ac2de85a0e
Jaykumar <patiljaykumar4.1@gmail.com>
2023-09-03 23:26:06 +0530

    modification in file

--- Addition.java
+++ Addition.java
@@ -2,7 +2,8 @@ public class Addition{
     public static void main(String[] args){
         int a = 1;
         int b = 2;
+        int c = 5;
 
-        System.out.println("Sum is "+(a+b));
+        System.out.println("Sum is "+(a+b+c));
     }
 }
